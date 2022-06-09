88e4ce61f740f51b635f5d5279fcc63576111776
Chengwen Liu <liuchw2010@gmail.com>
2022-06-09 16:22:25 -0500

    match to multiple templates

--- IP_MatchTXYZ.py
+++ IP_MatchTXYZ.py
@@ -153,11 +153,11 @@ def comparefingerprints(fp1, fp2):
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument('-t', dest = 'template', help = "Template txyz file", required=True)  
+  parser.add_argument('-t', dest = 'template', nargs='+', help = "Template txyz file(s)", required=True)  
   parser.add_argument('-d', dest = 'dealwith', help = "File to deal-with, can be .xyz/.txyz/.pdb", required=True)  
   parser.add_argument('-s', dest = 'sortatom', help = "sort atoms according to template file", default=False, type=bool)  
   args = vars(parser.parse_args())
-  template = args["template"]
+  templates = args["template"]
   dealwith  = args["dealwith"]
   global sortatom
   sortatom  = args["sortatom"]
@@ -176,27 +176,28 @@ if __name__ == "__main__":
     os.system(obstr)
   
   match = False
+  newidx = []
   fname = dealwith + "_2"
-  atoms1, coord1, _, types1, connections1 =  readTXYZ(dealwith)
-  atoms2, coord2, _, types2, connections2 =  readTXYZ(template)
-  if len(atoms1) == len(atoms2):
-    fp1 = fingerprint(template)
-    fp2 = fingerprint(dealwith)
-    match, newidx = comparefingerprints(fp1, fp2)
-    with open(fname, 'w') as f:
-      f.write("%3s\n"%len(atoms1))
-      for i in range(len(newidx)):
-        idx = int(newidx[i])
-        if sortatom:
-          f.write("%3s%3s%12.6f%12.6f%12.6f  %s   %s\n"%(i+1,atoms1[idx], coord1[idx][0], coord1[idx][1], coord1[idx][2], types2[i], '  '.join(connections2[i])))
-        else:
-          f.write("%3s%3s%12.6f%12.6f%12.6f  %s   %s\n"%(i+1,atoms1[i], coord1[i][0], coord1[i][1], coord1[i][2], types2[idx], '  '.join(connections1[i])))
-  if not match: 
-    print(f"Could not match {template} and {dealwith}")
-  else:
-    print(f"New file generated {dealwith}_2")
-  
-  with open("mapping.lst", 'w') as f:
-    for i in range(len(newidx)):
-      f.write("%6d%6d\n"%(i, newidx[i]))
-  print("The atom order mapping has been saved in mapping.lst")
+  for template in templates:
+    atoms1, coord1, _, types1, connections1 =  readTXYZ(dealwith)
+    atoms2, coord2, _, types2, connections2 =  readTXYZ(template)
+    if len(atoms1) == len(atoms2):
+      fp1 = fingerprint(template)
+      fp2 = fingerprint(dealwith)
+      match, newidx = comparefingerprints(fp1, fp2)
+    if not match: 
+      print(f"Could not match {template} and {dealwith}")
+    else:
+      print(f"Matched {template}. New file generated {dealwith}_2")
+      with open(fname, 'w') as f:
+        f.write("%3s\n"%len(atoms1))
+        for i in range(len(newidx)):
+          idx = int(newidx[i])
+          if sortatom:
+            f.write("%3s%3s%12.6f%12.6f%12.6f  %s   %s\n"%(i+1,atoms1[idx], coord1[idx][0], coord1[idx][1], coord1[idx][2], types2[i], '  '.join(connections2[i])))
+          else:
+            f.write("%3s%3s%12.6f%12.6f%12.6f  %s   %s\n"%(i+1,atoms1[i], coord1[i][0], coord1[i][1], coord1[i][2], types2[idx], '  '.join(connections1[i])))
+      with open("mapping.lst", 'w') as f:
+        for i in range(len(newidx)):
+          f.write("%6d%6d\n"%(i, newidx[i]))
+      print("The atom order mapping has been saved in mapping.lst")
